3fe271ab397e433cf687f3b3320d257fb2679f67
martinPandarski <marto65481@gmail.com>
2021-11-22 21:33:13 +0200

    feat: add search field

--- src/components/library/Library.jsx
+++ src/components/library/Library.jsx
@@ -1,5 +1,6 @@
 
-import { Grid } from "@mui/material"
+import { Grid, InputAdornment, TextField } from "@mui/material"
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import { useMediaQuery } from "@mui/material"
 import BookCard from "./BookCard"
 import styles from './Library.module.scss'
@@ -9,7 +10,18 @@ export default function Library() {
 
     return (
         <div className={styles.wrapper} >
-            <Grid flexDirection={isMobile ? "column" : "row"} spacing={1} container>
+            <div className={styles["search-wrapper"]}>
+                <h3>ALL BOOKS</h3>
+                <TextField
+                    size="small"
+                    classes={{ root: styles.search }}
+                    label="Search"
+                    InputProps={{
+                        endAdornment:
+                            (<InputAdornment position="end"><SearchOutlinedIcon /></InputAdornment>)
+                    }} />
+            </div>
+            <Grid className={styles.library} flexDirection={isMobile ? "column" : "row"} spacing={2} container>
                 <Grid item xl={6}>
                     <BookCard />
                 </Grid>
