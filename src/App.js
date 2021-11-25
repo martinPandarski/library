3daada4371fb912eccaedab3f03f4f05e98c3f93
martinPandarski <marto65481@gmail.com>
2021-11-25 20:03:43 +0200

    feat: add private route component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
-import { Outlet, Navigate } from "react-router-dom";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
 import Header from "./components/header/Header";
 import Library from "./components/library/Library";
 import BookDetails from "./components/library/BookDetails";
 import Settings from "./components/settings/Settings";
-
-// function PrivateOutlet() {
-//     const isLoggedIn = localStorage.getItem("token");
-//     return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
-// }
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
     const location = useLocation();
@@ -25,9 +20,30 @@ function App() {
                 <Route path="login" element={<Login />} />
                 <Route path="register" element={<Register />} />
 
-                <Route path="/" element={<Library />}></Route>
-                <Route path="/settings" element={<Settings />} />
-                <Route path="book/:id" element={<BookDetails />} />
+                <Route
+                    path="/"
+                    element={
+                        <PrivateRoute>
+                            <Library />
+                        </PrivateRoute>
+                    }
+                ></Route>
+                <Route
+                    path="/settings"
+                    element={
+                        <PrivateRoute>
+                            <Settings />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="book/:id"
+                    element={
+                        <PrivateRoute>
+                            <BookDetails />
+                        </PrivateRoute>
+                    }
+                />
             </Routes>
         </div>
     );
